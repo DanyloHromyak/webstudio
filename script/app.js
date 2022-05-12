6a16e5902e6d3f1f6094a88ec5d44dd74d50bcee
DanyloHromyak <93078858+DanyloHromyak@users.noreply.github.com>
2022-05-12 18:58:23 +0300

    modal margin fixed & navbar typography fixed

--- script/app.js
+++ script/app.js
@@ -8,7 +8,6 @@ function openModal() {
     contactModalElem.classList.add("modal_open");
   }, 0);
   document.body.style.overflow = "hidden";
-  document.body.style.marginRight = "17px";
   contactModalElem.querySelector("input").focus();
 }
 
@@ -16,12 +15,11 @@ function closeModal() {
   contactModalElem.classList.remove("modal_open");
   setTimeout(() => {
     contactModalElem.style.display = "none";
-    document.body.style.marginRight = "0";
     document.body.style.overflow = "auto";
   }, 300);
 }
 
-window.onkeyup = e => {
+window.onkeyup = (e) => {
   if (e.keyCode == 27) {
     closeModal();
   }
